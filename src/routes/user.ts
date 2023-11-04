6129207da60497a6bd34d6364c731c0544be5368
Don <neveradio@gmail.com>
2023-11-04 18:20:05 +0900

    [feat] patch user

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -20,17 +20,6 @@ router.get("/", authenticateToken, (req: Request, res: Response) => {
 router.patch("/", userValidationRules, validateUser, upload.single('images'), authenticateToken, (req: Request, res: Response) => {
   // authenticateToken 미들웨어에서 추가된 user 사용
   const user: User = req.user as User;
-
-  // 파일에서 사용자 목록을 읽어옴
-  const usersData = fs.readFileSync(usersFilePath, "utf8");
-  const users: User[] = JSON.parse(usersData);
-
-  // userId에 해당하는 사용자 찾기
-  const userIndex = users.findIndex(u => u.id === user.id);
-  if (userIndex === -1) {
-    return res.status(404).send({ message: "User not found" });
-  }
-
   // 파일 업로드가 있을 경우 이미지 URL 업데이트
   const updatedData = req.body;
   if (req.file) {
@@ -38,7 +27,13 @@ router.patch("/", userValidationRules, validateUser, upload.single('images'), au
   }
 
   // 사용자 정보 업데이트
-  users[userIndex] = { ...users[userIndex], ...updatedData };
+  Object.assign(user, updatedData);
+
+  // 업데이트된 사용자 정보를 파일에 다시 쓰기
+  const usersData = fs.readFileSync(usersFilePath, "utf8");
+  const users: User[] = JSON.parse(usersData);
+  const userIndex = users.findIndex(u => u.id === user.id);
+  users[userIndex] = user;
   fs.writeFileSync(usersFilePath, JSON.stringify(users));
 
   res.send({ message: "User profile updated successfully!" });
