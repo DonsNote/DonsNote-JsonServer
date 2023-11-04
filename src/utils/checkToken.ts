1b5fbc68c1716c70fc577f8164ba190e04355e79
Don <neveradio@gmail.com>
2023-11-04 10:31:40 +0900

    확인중

--- src/utils/checkToken.ts
+++ src/utils/checkToken.ts
@@ -1,5 +1,9 @@
 import jwt from "jsonwebtoken";
 
+if (!process.env.SECRET_KEY) {
+  throw new Error('SECRET_KEY is not defined');
+}
+
 const secret = process.env.SECRET_KEY as string;
 
 export const verifyToken = (token: string) => {
