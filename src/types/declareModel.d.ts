51955b851043bb8075a2c3095bef5c6fb05b8edc
Don <neveradio@gmail.com>
2023-11-04 10:04:43 +0900

    [feat] 되라

---
+++ src/types/declareModel.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    namespace Express {
+        interface Request {
+            user?: import('../models/userModel').User;
+            artist?: import('../models/artistModel').Artist;
+            busking?: import('../models/buskingModel').Busking;
+            member?: import('../models/memberModel').Member;
+        }
+    }
+}
+
+export { };
